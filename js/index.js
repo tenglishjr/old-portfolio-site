a69f4c8f94296ca97b1fd92beb7fc2a1eb8baece
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-07-26 21:19:53 -0500

    First commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-$(function () {
-   'use strict';
+$(document).ready( () => {
+    'use strict';
 
-    console.log('Hi there');
+    setTimeout(() => {
+        $('#name-header').fadeIn();
+        $('#job-title').fadeIn();
+    }, 500);
 
-    $('')
+    $(window).scroll(function(){
+        $(".down-arrow-icon").css("opacity", 1 - $(window).scrollTop() / 250);
+    });
+
+    $(window).resize(() => {
+        if ($(window).width() <= 576) {
+            $('aside').addClass('fixed-top');
+        }
+    });
 
 });
