56cda3bb2154b47ad360616514f055e945699e2f
TJ English <tenglishjr@TJs-MacBook-Air.local>
2018-08-18 18:42:51 -0500

    rough desktop

--- js/index.js
+++ js/index.js
@@ -7,45 +7,17 @@ $(document).ready( () => {
     }, 1000);
 
     setTimeout (() => {
-        $(".down-arrow-div").fadeIn(2000);
-    }, 2500);
+        $(".down-arrow-div").fadeIn(3000);
+    }, 5000);
 
     $(window).scroll( () => {
         $(".down-arrow-icon").css("opacity", 1 - $(window).scrollTop() / 450);
     });
 
-
-    // For Smooth Navigation...
-    $('a[href*="#"]')
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {  // Would not allow me to use `=>` for function (would break)
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                let target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 500, function() {     // Same here...
-                        let $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) {
-                            return false;
-                        } else {
-                            $target.attr('tabindex','-1');
-                            $target.stop();
-                            $target.focus();
-                        }
-                    });
-                }
-            }
-        });
+    $( "a.links" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
 
     // ====== STELLAR.JS ======
     // $(window).stellar();
